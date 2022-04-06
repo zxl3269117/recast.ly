157efd44915ac5ca75b8f857a4094fff5a350fb4
Ziqian Li <liziqian1992@gmail.com>
2022-04-05 21:12:01 -0700

    attempting Hook together the Video List and Video Player components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,60 @@ import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video = {exampleVideoData[0]}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos = {exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoList: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  updateState (video) {
+    // passing video inside this function as arg
+    // changing the state
+    // update this.state to
+    // currentVideo is the video got clicked
+    this.setState({currentVideo: video});
+  }
+
+  render() {
+    return (
+      <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer video = {exampleVideoData[0]}/>
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos = {exampleVideoData}/>
+//       </div>
+//     </div>
+//   </div>
+    );
+  }
+}
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer video = {exampleVideoData[0]}/>
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos = {exampleVideoData}/>
+//       </div>
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
