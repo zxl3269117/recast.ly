2cf3cd92131a5910856446a62b9941f17723a9ae
Ziqian Li <liziqian1992@gmail.com>
2022-04-05 20:28:45 -0700

    Create a dynamic Video List component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +14,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos = {exampleVideoData}/>
       </div>
     </div>
   </div>
@@ -19,3 +23,4 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,11 @@
-import exampleVideoData from '/src/data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div>
-      <h5>
-        <em>
-          {exampleVideoData.map((video) => {
-            <videoListEntry
-            />;
-          })}
-        </em>
-        view goes here
-      </h5>
-    </div>
+    {props.videos.map(video =>
+      <VideoListEntry video = {video} />
+    )}
   </div>
 );
 
